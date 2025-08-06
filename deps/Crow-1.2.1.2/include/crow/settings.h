1e7083f2cc8feb81191f048daac0f902794baac2
Zvend <soerenganske@hotmail.de>
2025-08-06 23:55:19 +0200

    turned off crow logging

--- deps/Crow-1.2.1.2/include/crow/settings.h
+++ deps/Crow-1.2.1.2/include/crow/settings.h
@@ -6,7 +6,7 @@
 //#define CROW_ENABLE_DEBUG
 
 /* #ifdef - enables logging */
-#define CROW_ENABLE_LOGGING
+//#define CROW_ENABLE_LOGGING
 
 /* #ifdef - enforces section 5.2 and 6.1 of RFC6455 (only accepting masked messages from clients) */
 //#define CROW_ENFORCE_WS_SPEC
